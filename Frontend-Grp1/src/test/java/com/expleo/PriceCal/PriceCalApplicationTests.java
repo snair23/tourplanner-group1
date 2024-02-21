fe6887bca6de10b0dba3787302e72dc8f7c57472
snair23 <157672521+snair23@users.noreply.github.com>
2024-02-21 14:39:39 +0530

    Add files via upload

---
+++ Frontend-Grp1/src/test/java/com/expleo/PriceCal/PriceCalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.expleo.PriceCal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PriceCalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
